599018946993c9e0107e0b98957c0222cb19e4cb
mankatos <klaatus@msn.com>
2024-10-14 14:25:30 +0900

    1.작성자: 황재인 2.작업 내용: 로그인시 ERR_NETWORK  처리 코드 추가 3.작성일: 2024-10-14

--- src/api/loginApi.js
+++ src/api/loginApi.js
@@ -27,6 +27,12 @@ export const authLogin = async (email, password) => {
         return response.data;
     } catch (error) {
 
+        if (error.code === 'ERR_NETWORK') {
+            alert('서버와의 연결에 문제가 발생했습니다. 인터넷 연결을 확인하거나 다시 시도해주세요.');
+        }else{
+            alert("로그인 중 오류가 발생했습니다");
+        }
+
         console.error("로그인 중 오류가 발생했습니다:", error);
         throw error; // 에러를 다시 던져 호출자가 처리할 수 있도록 함
     }
